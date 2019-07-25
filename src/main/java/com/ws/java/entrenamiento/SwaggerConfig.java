932b1a405d7db1bd750ce48b2ac7cdbb89951445
Juan Carlos Hernandez <juancho061899@gmail.com>
2019-07-25 11:44:52 -0500

    tiquet4 - documentando el servicio de consulta con swagger

--- src/main/java/com/ws/java/entrenamiento/SwaggerConfig.java
+++ src/main/java/com/ws/java/entrenamiento/SwaggerConfig.java
@@ -13,7 +13,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 @Bean
-public Docket api() {
-return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
-}
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
