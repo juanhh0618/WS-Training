58e5a6ee57f7951ee8564a15ae837657a25abde0
Juan Carlos Hernandez <juancho061899@gmail.com>
2019-07-25 09:14:59 -0500

    tiquet2 - Se agrego servicio de login
    El servicio de login es un rest,por metodo post,que valida la informacion en un archivo json

---
+++ src/main/java/com/ws/java/entrenamiento/model/LoginModel.java
@@ -0,0 +1,65 @@
+package com.ws.java.entrenamiento.model;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Serializable;
+import java.text.ParseException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class LoginModel implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3560972546182458142L;
+	private String user;
+	private String password;
+	private String name;
+	private String lastAccess;
+	private String status;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastAccess() {
+		return lastAccess;
+	}
+
+	public void setLastAccess(String lastAccess) {
+		this.lastAccess = lastAccess;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+	
+}
