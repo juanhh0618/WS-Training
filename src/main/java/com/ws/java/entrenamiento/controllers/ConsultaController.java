51204a787a56a96539480ded78b9d8632c40150b
Juan Carlos Hernandez <juancho061899@gmail.com>
2019-07-26 10:29:06 -0500

    tiquet4 - se dividio el codigo del controlador de consulta
    se particiono el codigo para que el controlador quedara solo con la responsabilidad de exponer el servicio

--- src/main/java/com/ws/java/entrenamiento/controllers/ConsultaController.java
+++ src/main/java/com/ws/java/entrenamiento/controllers/ConsultaController.java
@@ -1,19 +1,17 @@
 package com.ws.java.entrenamiento.controllers;
 
-import org.springframework.http.MediaType;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.FileReader;
+
 import com.ws.java.entrenamiento.models.ConsultaModel;
 
 import io.swagger.annotations.Api;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.ws.java.entrenamiento.services.ConsultaService;
 
 
 @Api(tags = "Consulta")
@@ -28,43 +26,15 @@ public class ConsultaController {
 			produces = "application/json") //Indicamos que produce json
 			//consumes = {MediaType.APPLICATION_XML_VALUE},
 			//produces = {MediaType.APPLICATION_XML_VALUE})
-	public @ResponseBody ConsultaModel validateUser(@RequestBody ConsultaModel user) throws Exception {
-		ConsultaModel response = new ConsultaModel();
+	
+		public @ResponseBody ConsultaModel PublishService(@RequestBody ConsultaModel userid) throws Exception {
 		
-		response.setMessage("Invalid User");
-		System.out.println("User: " + user.getUserid());
+		ConsultaService response = new ConsultaService();
 		
+		System.out.println("User: " + userid.getUserid());
 		
-		
-		try {		
-				JSONParser parser = new JSONParser();			
-				Object obj = parser.parse(new FileReader("C:\\Users\\JUANCARLOSHERNANDEZH\\Desktop\\informacion.json"));				
-				JSONObject jsonObject = (JSONObject) obj;
-			    JSONArray listUsers =  (JSONArray) jsonObject.get("usuarios");
-			    
-			    listUsers.get(1);
-				
-			   for (int i = 0; i < listUsers.size(); i++) {	
-				   
-				jsonObject = (JSONObject) listUsers.get(i);   
-				if (user.getUserid().equals(jsonObject.get("userid").toString())) {
-				response.setUserid(jsonObject.get("userid").toString());
-				response.setNombre(jsonObject.get("name").toString());
-				response.setId(jsonObject.get("id").toString());
-				response.setIp(jsonObject.get("ip").toString());
-				response.setCapagna(jsonObject.get("campagna").toString());
-				response.setFecha(jsonObject.get("fecha").toString());
-				response.setMessage("User Valid");
-				
-				}
-		    }
-				
-			return response;
+		ConsultaModel respuesta = response.method(userid);
 			
-		} catch (Exception ex) {
-			
-			return response;
-		}
+		return respuesta;
 	}
-
 }
---
+++ src/main/java/com/ws/java/entrenamiento/services/ConsultaService.java
@@ -0,0 +1,47 @@
+package com.ws.java.entrenamiento.services;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.ws.java.entrenamiento.models.ConsultaModel;
+
+public class ConsultaService {
+
+	public ConsultaModel method(ConsultaModel userid) {
+		ConsultaModel respuesta = new ConsultaModel();
+			try {		
+				JSONParser parser = new JSONParser();			
+				Object obj = parser.parse(new FileReader("C:\\Users\\JUANCARLOSHERNANDEZH\\Desktop\\informacion.json"));				
+				JSONObject jsonObject = (JSONObject) obj;
+			    JSONArray listUsers =  (JSONArray) jsonObject.get("usuarios");
+			    
+			    listUsers.get(1);
+				
+			   for (int i = 0; i < listUsers.size(); i++) {	
+				   
+				jsonObject = (JSONObject) listUsers.get(i);   
+				if (userid.getUserid().equals(jsonObject.get("userid").toString())) {
+					respuesta.setUserid(jsonObject.get("userid").toString());
+					respuesta.setNombre(jsonObject.get("name").toString());
+					respuesta.setId(jsonObject.get("id").toString());
+					respuesta.setIp(jsonObject.get("ip").toString());
+					respuesta.setCapagna(jsonObject.get("campagna").toString());
+					respuesta.setFecha(jsonObject.get("fecha").toString());
+					respuesta.setMessage("User Valid");
+				
+				}
+		    }
+				
+			return respuesta;
+			
+		} catch (Exception ex) {
+			
+			return respuesta;
+		
+	}
+}
+}
+
