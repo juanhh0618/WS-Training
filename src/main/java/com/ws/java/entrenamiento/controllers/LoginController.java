1d2d38d5ba135385d37e19c9d5e42a99cf972b43
Juan Carlos Hernandez <juancho061899@gmail.com>
2019-07-26 14:37:07 -0500

    tiquet6 - se agregaron comentarios a las clases
    Se documento cada clase y se hicieron una serie de comentarios para hacerlas mas entendibles

--- src/main/java/com/ws/java/entrenamiento/controllers/LoginController.java
+++ src/main/java/com/ws/java/entrenamiento/controllers/LoginController.java
@@ -2,13 +2,6 @@ package com.ws.java.entrenamiento.controllers;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
-/**
- * 
- * @author JUANCARLOSHERNANDEZH
- * @since 23/07/2019
- * @category Controller
- * In this controller we are publish the web service
- */
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +12,14 @@ import com.ws.java.entrenamiento.models.LoginModel;
 import io.swagger.annotations.Api;
 
 import com.ws.java.entrenamiento.services.LoginService;
+
+/**
+ * 
+ * @author JUANCARLOSHERNANDEZH
+ * @since 23/07/2019
+ * @category Controller
+ * In this controller we are publish the web service
+ */
 @Api(tags = "Login")
 @RestController //Le decimos que es un rest controller
 @RequestMapping(path = "/servicesREST/login") //Ruta para encontrar el servicio
