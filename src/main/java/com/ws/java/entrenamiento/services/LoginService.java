2c57fd9161662590ce1d139fcbc2aca33dd569e1
Juan Carlos Hernandez <juancho061899@gmail.com>
2019-07-26 15:25:44 -0500

    tiquet6 - Se agrego una etiqueta
    En la clase de LoginService se especifico por medio de una etiqueta(@Service) que era un servicio

--- src/main/java/com/ws/java/entrenamiento/services/LoginService.java
+++ src/main/java/com/ws/java/entrenamiento/services/LoginService.java
@@ -12,11 +12,14 @@ import java.io.FileReader;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.stereotype.Service;
 
 import com.ws.java.entrenamiento.models.LoginModel;
 
+@Service
 public class LoginService {
 
+	
 	public LoginModel validateUser(LoginModel user) {
 		LoginModel respuesta = new LoginModel();
 		respuesta.setStatus("Invalid User");
